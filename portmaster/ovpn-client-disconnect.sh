f3a1ac212ce6af7265a9fd83b2cb5381192e70c4
Pronto <pro_to@mail.ru>
2025-07-24 23:58:07 +0300

    global cleaning
    removed  unused trash

--- portmaster/ovpn-client-disconnect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get client's IP
-CLIENT_IP=$ifconfig_pool_remote_ip
-
-# Send message to portmaster
-echo "DISCONNECT: $CLIENT_IP" | nc -w 1 $PORTMASTER_IP $PORTMASTER_PORT
--- portmaster/wg-client-disconnect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Arg — client's IP
-CLIENT_IP=$1
-
-# Send message to portmaster
-echo "DISCONNECT: $CLIENT_IP" | nc -w 1 $PORTMASTER_IP $PORTMASTER_PORT
